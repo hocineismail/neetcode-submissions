0e04a30053143cc71b7458081556cac254beb42e
Ismail Hocine <hocineismail75@gmail.com>
2026-09-17 15:54:31 +0100

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.js
@@ -0,0 +1,29 @@
+class Solution {
+    evalRPN(tokens) {
+        const stack = [];
+
+        for (const c of tokens) {
+            if (c === "+") {
+                stack.push(stack.pop() + stack.pop());
+
+            } else if (c === "-") {
+                const a = stack.pop();
+                const b = stack.pop();
+                stack.push(b - a);
+
+            } else if (c === "*") {
+                stack.push(stack.pop() * stack.pop());
+
+            } else if (c === "/") {
+                const a = stack.pop();
+                const b = stack.pop();
+                stack.push(Math.trunc(b / a));
+
+            } else {
+                stack.push(Number(c));
+            }
+        }
+
+        return stack[0];
+    }
+}
